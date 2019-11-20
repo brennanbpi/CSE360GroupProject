1b33a3c5531d58d0e734dd271cfeb9b69baa9223
brennanbpi <56239675+brennanbpi@users.noreply.github.com>
2019-11-20 16:56:21 -0700

    basic start to the project

---
+++ gradeAnalyzer.java
@@ -0,0 +1,11 @@
+
+import javax.swing.*;// gives us access 
+
+//this will be the main driver of the program
+public class Main 
+{
+int low;//lowest possible grade
+int high;//Highest possible grade
+
+
+}
