e7ca4aaf47023023001294a37f99d5e98e47db14
Jacob Ybarra <jacobybarra13@yahoo.com>
2019-12-04 11:50:55 -0700

    Upated backend; Filled in the remaning methods.

--- CSE360Group/src/gropProject/backend.java
+++ CSE360Group/src/gropProject/backend.java
@@ -1,5 +1,6 @@
 package gropProject;
 
+import java.io.*;
 import java.util.*;
 
 public class backend
@@ -16,13 +17,35 @@ public class backend
 		low = l;//load in lowerbound
 		high = h;//load in upperbound
 		log = "";//begin log
-		ArrayList<Float> allgrades = new ArrayList<Float>();//
+		allgrades = new ArrayList<Float>();
 	}
 	
 	//takes string filename to read in files from file
 	public void loadfile(String filename)
 	{
-		allgrades.clear();
+		allgrades.clear();									//Clears ArrayList for new set of data
+		File data = new File(filename);						//Creates File using name
+		if(data.isFile())									//Ensures that, there is a file using the name
+		{
+			BufferedReader br = new BufferedReader(data);
+			String str;
+			if(filename.endsWith(".txt"))					//Checks if file is .txt...
+			{
+				while((str = br.readLine()) != null)		//Reads every line of file...
+					allgrades.add(Float.parseFloat(str));	//... ands adds it to the ArrayList
+			}
+			else											//... or .csv
+			{
+				while((str = br.readLine()) != null)
+					{
+						String[] temp = str.split(",");		//Saves line of CSVs
+						for(int i = 0; i < temp.length; i++)//Goes through the line of CSVs...
+							allgrades.add(Float.parseFloat(temp[i]));	//... and adds them to ArrayList
+					}
+			}
+		}
+		else												//If there is no file, prints error
+			System.out.println("Error: File Not Found.");
 	}
 	
 	//adds value x to the arrayList
@@ -34,7 +57,7 @@ public class backend
 	//deletes first appearance of a value from allgrades
 	public void deleteValue(float delThis)
 	{
-		for(int trav=0; trav<allgrades.size()-1; trav++)
+		for(int trav=0; trav<=allgrades.size()-1; trav++)
 		{
 			if(allgrades.get(trav)==delThis)
 			{
@@ -60,33 +83,62 @@ public class backend
 	// returns mean of data set
 	public float getMean()
 	{
-		return 0;
+		float sum = 0;
+		for(int i = 0; i < allgrades.size(); i++)
+			sum += allgrades.get(i);
+		return sum/allgrades.size();
 	}
 	
 	//returns high of data set
 	public float getHigh()
 	{
-		return 0;
+		float max = 0;
+		for(int i = 0; i < allgrades.size(); i++)
+			if(max < allgrades.get(i))
+				max = allgrades.get(i);
+		return max;
+	}
+
+	//returns low of data set
+	public float getLow()
+	{
+		float low = 0;
+		for(int i = 0; i < allgrades.size(); i++)
+			if(low > allgrades.get(i))
+				low = allgrades.get(i);
+		return low;
 	}
 	
 	//returns median of data set
 	public float getMedian()
 	{
-		return 0;	
+		int median;
+		median = (allgrades.size()/2 + 0.5);	//Will take ceiling. Ex: 3/2 = 1.5 + 0.5 = 2 ; 4/2 = 2 + 0.5 = 2 becuase int drops .05
+		return allgrades.get(median);
 	}
 	
 	//returns mode of data sets
 	public float getMode()
 	{
-		return 0;
+		int i, occur = 0;
+		float num, absoccur = 0, mode = 0;
+		ArrayList<Float> temp = allgrades.clone();//Clones allgrades to not intefere with original data
+		while(temp.size() != 0)
+		{
+			num = temp.get(1);						//Gets the float of the first index; will be different because original 1 gets removed.
+			while((i = temp.indexOf(num)) != -1)	//-1 is index not found, searches for indexes equal to value of index one
+			{
+				temp.remove(i);						//First run will remove index 1; Subsequent runs will remove all other occurences of the value
+				occur++;							//To help decide which appears most
+			}
+			if(occur > absoccur)					//Checks current iterations mode to the absolute mode
+			{
+				absoccur = occur;					//If current iteration is greater, than current is now absolute
+				mode = num;							//new mode, equal to current value
+			}
+			occur = 0;								//Resets occurences
+		}
+		return mode;								//Returns value with highest occurences
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 	
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CSE360GroupProject
 this is our group project
+
+    Albert De La Cruz
+    Matt Fleming
+    Brennan Iannone
+    Jacob Ybarra
