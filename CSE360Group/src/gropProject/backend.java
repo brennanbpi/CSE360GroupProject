a23a4786775b714c5e0aeb83673ef09a286b4132
Jacob Ybarra <jacobybarra13@yahoo.com>
2019-12-04 12:19:23 -0700

    Fixed a few mistakes, In my changes to backend.

--- CSE360Group/src/gropProject/backend.java
+++ CSE360Group/src/gropProject/backend.java
@@ -23,28 +23,28 @@ public class backend
 	//takes string filename to read in files from file
 	public void loadfile(String filename)
 	{
-		allgrades.clear();									//Clears ArrayList for new set of data
-		File data = new File(filename);						//Creates File using name
-		if(data.isFile())									//Ensures that, there is a file using the name
+		allgrades.clear();	//Clears ArrayList for new set of data
+		File data = new File(filename);	//Creates File using name
+		if(data.isFile())	//Ensures that, there is a file using the name
 		{
 			BufferedReader br = new BufferedReader(data);
 			String str;
-			if(filename.endsWith(".txt"))					//Checks if file is .txt...
+			if(filename.endsWith(".txt"))	//Checks if file is .txt...
 			{
-				while((str = br.readLine()) != null)		//Reads every line of file...
+				while((str = br.readLine()) != null)	//Reads every line of file...
 					allgrades.add(Float.parseFloat(str));	//... ands adds it to the ArrayList
 			}
-			else											//... or .csv
+			else	//... or .csv
 			{
 				while((str = br.readLine()) != null)
 					{
-						String[] temp = str.split(",");		//Saves line of CSVs
+						String[] temp = str.split(",");	//Saves line of CSVs
 						for(int i = 0; i < temp.length; i++)//Goes through the line of CSVs...
 							allgrades.add(Float.parseFloat(temp[i]));	//... and adds them to ArrayList
 					}
 			}
 		}
-		else												//If there is no file, prints error
+		else	//If there is no file, prints error
 			System.out.println("Error: File Not Found.");
 	}
 	
@@ -92,7 +92,7 @@ public class backend
 	//returns high of data set
 	public float getHigh()
 	{
-		float max = 0;
+		float max = allgrades.get(0);
 		for(int i = 0; i < allgrades.size(); i++)
 			if(max < allgrades.get(i))
 				max = allgrades.get(i);
@@ -102,7 +102,7 @@ public class backend
 	//returns low of data set
 	public float getLow()
 	{
-		float low = 0;
+		float low = allgrades.get(0);
 		for(int i = 0; i < allgrades.size(); i++)
 			if(low > allgrades.get(i))
 				low = allgrades.get(i);
@@ -113,7 +113,7 @@ public class backend
 	public float getMedian()
 	{
 		int median;
-		median = (allgrades.size()/2 + 0.5);	//Will take ceiling. Ex: 3/2 = 1.5 + 0.5 = 2 ; 4/2 = 2 + 0.5 = 2 becuase int drops .05
+		median = ((int)(allgrades.size()/2 + 0.5 - 1));	//Will take ceiling. Ex: 3/2 = 1.5 + 0.5 = 2 ; 4/2 = 2 + 0.5 = 2 becuase int drops .05
 		return allgrades.get(median);
 	}
 	
@@ -125,20 +125,20 @@ public class backend
 		ArrayList<Float> temp = allgrades.clone();//Clones allgrades to not intefere with original data
 		while(temp.size() != 0)
 		{
-			num = temp.get(1);						//Gets the float of the first index; will be different because original 1 gets removed.
+			num = temp.get(0);	//Gets the float of the first index; will be different because original 1 gets removed.
 			while((i = temp.indexOf(num)) != -1)	//-1 is index not found, searches for indexes equal to value of index one
 			{
-				temp.remove(i);						//First run will remove index 1; Subsequent runs will remove all other occurences of the value
-				occur++;							//To help decide which appears most
+				temp.remove(i);	//First run will remove index 1; Subsequent runs will remove all other occurences of the value
+				occur++;	//To help decide which appears most
 			}
-			if(occur > absoccur)					//Checks current iterations mode to the absolute mode
+			if(occur > absoccur)	//Checks current iterations mode to the absolute mode
 			{
-				absoccur = occur;					//If current iteration is greater, than current is now absolute
-				mode = num;							//new mode, equal to current value
+				absoccur = occur;	//If current iteration is greater, than current is now absolute
+				mode = num;	//new mode, equal to current value
 			}
-			occur = 0;								//Resets occurences
+			occur = 0;	//Resets occurences
 		}
-		return mode;								//Returns value with highest occurences
+		return mode;	//Returns value with highest occurences
 	}
 	
 }
